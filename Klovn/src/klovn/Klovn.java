1f10986bf3a04e40f4c0a99431fc7f17ca3c3832
pølsehorn <>
2018-11-13 15:06:52 +0100

    howdy

--- Klovn/src/klovn/Klovn.java
+++ Klovn/src/klovn/Klovn.java
@@ -16,7 +16,7 @@ public class Klovn {
      */
     public static void main(String[] args) {
         System.out.println("howdy");
-        System.out.println("partner");
+        System.out.println("homer");
     }
     
 }
